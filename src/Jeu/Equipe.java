4259e8afb791a32c9c00e4b6d15684a4a17e92cf
LexandMR <lexandmv@gmail.com>
2026-01-29 16:25:02 +0100

    Commit class Equipe

--- src/Jeu/Equipe.java
+++ src/Jeu/Equipe.java
@@ -1,7 +1,6 @@
 package Jeu;
 import java.util.*;
-public class Equipe
-{
+public class Equipe {
     private String joueur_1;
     private String joueur_2;
     private String joueur_3;
@@ -10,10 +9,58 @@ public class Equipe
     private int numEquipe;
 
 
-    public Equipe(String nomEquipe, int numEquipe)
-    {
+    public Equipe(String nomEquipe, int numEquipe) {
         this.nomEquipe = nomEquipe;
         this.numEquipe = numEquipe;
     }
 
+//Geteur/Seteur
+    public String getJoueur_1 ()
+    {
+        return joueur_1;
+    }
+    public String getJoueur_2 ()
+    {
+        return joueur_2;
+    }
+    public String getJoueur_3 ()
+    {
+        return joueur_3;
+    }
+    public String getJoueur_4 ()
+    {
+        return joueur_4;
+    }
+    public String getNomEquipe ()
+    {
+        return nomEquipe;
+    }
+    public int getNumEquipe ()
+    {
+        return numEquipe;
+    }
+    public void setJoueur_1 (String joueur_1)
+    {
+        this.joueur_1 = joueur_1;
+    }
+    public void setJoueur_2 (String joueur_2)
+    {
+        this.joueur_2 = joueur_2;
+    }
+    public void setJoueur_3 (String joueur_3)
+    {
+        this.joueur_3 = joueur_3;
+    }
+    public void setJoueur_4 (String joueur_4)
+    {
+    this.joueur_4 = joueur_4;
+    }
+    public void setNomEquipe (String nomEquipe)
+    {
+        this.nomEquipe = nomEquipe;
+    }
+    public void setNumEquipe ( int numEquipe)
+    {
+    this.numEquipe = numEquipe;
+    }
 }
