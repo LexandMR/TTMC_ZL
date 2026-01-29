afc717442400fa3f74b63c407c8143b21910d2d9
LexandMR <lexandmv@gmail.com>
2026-01-29 16:02:50 +0100

    Commit class Equipe

---
+++ src/Jeu/Equipe.java
@@ -0,0 +1,19 @@
+package Jeu;
+import java.util.*;
+public class Equipe
+{
+    private String joueur_1;
+    private String joueur_2;
+    private String joueur_3;
+    private String joueur_4;
+    private String nomEquipe;
+    private int numEquipe;
+
+
+    public Equipe(String nomEquipe, int numEquipe)
+    {
+        this.nomEquipe = nomEquipe;
+        this.numEquipe = numEquipe;
+    }
+
+}
